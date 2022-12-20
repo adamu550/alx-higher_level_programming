679c1c75fc9003a1c7f2a07c96363cce1feb9ed3
adam <aadamu7@yahoo.com>
2022-12-20 17:22:28 +0100

    tasks on data structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,59 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
-
+#include"lists.h"
 /**
- * is_palindrome - Check if a linked list is a palindrome
- * @head: The list
- *
- * Return: 1 if it's a palindrome, 0 otherwise
+ *is_palindrome - is palindrome
+ *@head: head
+ *Return: 1 if palindrome else 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *a = *head;
-	listint_t *b = *head;
+	listint_t *end, *beg;
+	int i = 0, j = 0;
 
-	if (*head == NULL)
+	end = *head;
+	beg = *head;
+
+	if (!*head)
 		return (1);
-
-	while (b && b->next && b->next->next)
+	while (end->next)
 	{
-		a = a->next;
-		b = b->next->next;
+		i++;
+		end = end->next;
 	}
-
-	a = reverse_list(&a);
-	b = *head;
-	while (a && b)
+	while (end->n == beg->n)
 	{
-		if (a->n != b->n)
-			return (0);
-		a = a->next;
-		b = b->next;
+		if (end == beg && j >= (i / 2))
+			return (1);
+		end--;
+		beg++;
+		j++;
 	}
-
-	return (1);
-}
-
-/**
- * reverse_list - Reverse a linked list
- * @head: The list
- *
- * Return: Pointer to the new head
- */
-listint_t *reverse_list(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
-
-	while (*head)
-	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
-	}
-
-	*head = prev;
-	return (*head);
+	return (0);
 }
